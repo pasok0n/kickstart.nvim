d81b9031fcb7f7727385cd54b43a263c92bebeaa
pasok0n <117275165+pasok0n@users.noreply.github.com>
2026-06-12 13:23:45 +0200

    changed theme to catppuccin latte

--- init.lua
+++ init.lua
@@ -382,18 +382,19 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'folke/tokyonight.nvim' }
+  vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } }
   ---@diagnostic disable-next-line: missing-fields
-  require('tokyonight').setup {
+  require('catppuccin').setup {
+    transparent_background = true,
     styles = {
-      comments = { italic = false }, -- Disable italics in comments
+    --  comments = { italic = false }, -- Disable italics in comments
     },
   }
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'catppuccin-latte'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
